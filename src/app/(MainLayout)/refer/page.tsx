c50d53c2fdad88e3ceb87f9e9790aa33e4c9aac0
rayhan108 <raihanshorker56@gmail.com>
2025-08-31 11:46:50 +0600

    fix issues

--- src/app/(MainLayout)/refer/page.tsx
+++ src/app/(MainLayout)/refer/page.tsx
@@ -87,7 +87,7 @@ export default function ReferPage() {
                       <input
                         type="text"
                         placeholder="Type Refer Code "
-                        {...register("code", {
+                        {...register("code",{
                           required: "Refer Code is required",
                         })}
                         className="w-full pl-10 pr-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none text-gray-700"
